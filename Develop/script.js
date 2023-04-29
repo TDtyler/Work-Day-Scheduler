c475993ba27c10d9f6e7eb0008671317be137911
TDTX 1 <tdtx1@TDTXs-MacBook-Pro.local>
2023-04-28 22:40:27 -0700

    text entry()

--- Develop/script.js
+++ Develop/script.js
@@ -10,4 +10,25 @@ function hourColor() {
 
     });
 }
+
+function textEntry() {
+    $('saveBtn').on('click', function() {
+        const key = $(this).parent().attr('id');
+        const value = $(this).children('description').val();
+        localStorage.setItem(key, value);
+    });
+}
+
+function reloadColor() {
+    $('.time-block').each(function() {
+        const hourBlock = parseInt(this.id);
+        if(hourBlock == currentHour) {
+            $(this).removeClass('past, future').addClass('present');
+        } else if (hourBlock < currentHour) {
+            $(this).removeClass('future, present').addClass('past');
+        } else {
+            $(this).removeClass(past, present).addClass('future');
+        }
+    });
+}
 })
