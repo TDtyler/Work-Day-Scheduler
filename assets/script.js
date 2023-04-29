a06679aa6297d23fde09a4f293e9708c6cbafee1
TDTX 1 <tdtx1@TDTXs-MacBook-Pro.local>
2023-04-29 15:06:42 -0700

    added description to readme

--- assets/script.js
+++ assets/script.js
@@ -9,7 +9,7 @@ function hourColor() {
     $('.time-block').each(function() {
         const hourBlock = parseInt(this.id);
         $(this).toggleClass('past', hourBlock < currentHour);
-        $(this).toggleClass('present', hourBlock === currentHour);
+        $(this).toggleClass('present', hourBlock == currentHour);
         $(this).toggleClass('future', hourBlock > currentHour);
     });
 }
